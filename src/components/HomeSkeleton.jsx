5ce8d6260f646862194dccb720eaecdaeb5e287e
Lindahehexd <tkd09152129@gmail.com>
2023-02-24 00:00:26 +0800

    modify the skeleton length in sm screen

--- src/components/HomeSkeleton.jsx
+++ src/components/HomeSkeleton.jsx
@@ -4,9 +4,9 @@ import { Skeleton, Stack, VStack } from "@chakra-ui/react";
 const HomeSkeleton = () => {
   return (
     <Stack  spacing={6}>
-      <Skeleton height='30px' w='700px' />
-      <Skeleton height='30px' w='700px' />
-      <Skeleton height='30px' w='700px' />
+      <Skeleton height='30px' w={{sm:'350px', lg:'700px'}} />
+      <Skeleton height='30px' w={{sm:'350px', lg:'700px'}} />
+      <Skeleton height='30px' w={{sm:'350px', lg:'700px'}} />
     </Stack>
   );
 };
